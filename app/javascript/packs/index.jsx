4b3b75a5f41ee96918093b54d6d097cd566e6de5
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-08-03 10:24:48 +0200

    connect store

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -5,6 +5,7 @@ import { App } from '../src/containers/app'
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import guitarsReducer from '../src/reducers/guitars_reducer'
+import { GuitarStore } from '../src/stores/guitar_store';
 
 const reducers = combineReducers({
   guitars: guitarsReducer
@@ -13,7 +14,8 @@ const reducers = combineReducers({
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Provider store={createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
-      <App/>
+      {/* <App/> */}
+      <GuitarStore/>
     </Provider>,
     document.body.appendChild(document.createElement('div')),
   )
--- app/javascript/src/containers/app.jsx
+++ app/javascript/src/containers/app.jsx
@@ -3,36 +3,6 @@ import PropTypes from 'prop-types'
 import { GuitarList } from './guitar_list'
 import { GuitarShow } from './guitar_show'
 
-const GUITAR_API = [
-{
-"id": 3,
-"brand": "Ibanez",
-"name": "JEM77BFP",
-"year": 1991,
-"user_id": 1,
-"created_at": "2022-08-02T14:13:16.664Z",
-"updated_at": "2022-08-02T14:13:16.664Z"
-},
-{
-"id": 2,
-"brand": "Music Man",
-"name": "JP6",
-"year": 2001,
-"user_id": 1,
-"created_at": "2022-08-02T14:13:16.656Z",
-"updated_at": "2022-08-02T14:13:16.656Z"
-},
-{
-"id": 1,
-"brand": "Fender",
-"name": "Stratocaster",
-"year": 2002,
-"user_id": 1,
-"created_at": "2022-08-02T14:13:16.647Z",
-"updated_at": "2022-08-02T14:13:16.647Z"
-}
-]
-
 export class App extends React.Component {
   constructor (props) {
     super (props)
@@ -44,7 +14,7 @@ export class App extends React.Component {
 
   guitarSelect (id) {
     let selected_guitar
-    GUITAR_API.forEach(guitar => {
+    this.props.guitars.forEach(guitar => {
       if (guitar.id == id) {
         selected_guitar = guitar
       }
@@ -57,7 +27,7 @@ export class App extends React.Component {
   render () {
     return <div className='container app__container'>
       <div className="left__window">
-        <GuitarList guitars={GUITAR_API} guitarSelect={this.guitarSelect}/>
+        <GuitarList guitars={this.props.guitars} guitarSelect={this.guitarSelect}/>
       </div>
       <div className="right__window">
         <GuitarShow guitar={this.state.selected_guitar}/>
