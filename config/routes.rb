94ba2566ac731d1c1d5956bd72c07e1b184dffb7
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-08-12 16:45:46 +0200

    move index guitar to home

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :guitars
   resources :pickups
   devise_for :users
-  root to: "guitars#index"
+  root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
